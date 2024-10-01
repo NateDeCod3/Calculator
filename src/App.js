5ddffac39a5a893d67aa8d4d4acddaea0f74ce0e
Kristenz24 <kristenzmingoy@gmail.com>
2024-10-01 11:49:48 +0800

    calcu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import { useState } from "react";
 
-function Display() {
+function Display({ display }) {
   return(
-    <>0</>
+    <>{display}</>
   );
 }
 
@@ -14,39 +15,101 @@ function Key({label, onClick}) {
   );
 }
 
-
-
 function App() {
 
-  const clickHandler =(e) => {
+  const [disp, setDisp] = useState(0);
+  const [num1, setNum1] = useState(null);
+  const [num2, setNum2] = useState(null);
+  const [op, setOp] = useState(null);
+
+
+// 
+
+
+  const numClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    console.log(value);
+    if (op===null) {
+      if (num1===null){
+        setNum1(value);
+        setDisp(value);
+      }else{
+        setNum1(num1+value);
+        setDisp(num1 + value);
+
+      }
+    }else{
+      if (num2===null){
+        setNum2(value);
+        setDisp(value);
+      }else{
+        setNum2(num2+value);
+        setDisp(num2 + value);
+
+      }
+    }
+    console.log(num1 + '|' + op + '|' + num2);
   }
+
+  const opClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    setOp(value);
+    setDisp(value);
+    console.log(num1 + '|' + op + '|' + num2);
+  }
+
+  const eqClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    console.log(num1 + '|' + op + '|' + num2);
+    if (op==="+"){
+      setDisp(parseInt(num1) + parseInt(num2));
+    }else if (op==="-"){
+      setDisp(parseInt(num1) - parseInt(num2));
+    }else if (op==="×"){
+      setDisp(parseInt(num1) * parseInt(num2));
+    }else if (op==="÷"){
+      setDisp(parseInt(num1) / parseInt(num2));
+    }else{
+      setDisp("ERROR");
+    }
+  }
+
+  const clrClickHandler = (e) => {
+    setNum1(null);
+    setNum2(null);
+    setOp(null);
+    setDisp(0);
+  }
+
+
+// 
+  
 
   return (
     <div className="App">
       <div className="Calc">
-        <div className="Display">
-          <Display />
+        <div className="Disp">
+          <Display display={disp} />
         </div>
         <div className="Buttons">
-          <Key label={7} onClick={clickHandler}/>
-          <Key label={8} onClick={clickHandler}/>
-          <Key label={9} onClick={clickHandler}/>
-          <Key label={"÷"} onClick={clickHandler}/>
-          <Key label={4} onClick={clickHandler}/>
-          <Key label={5} onClick={clickHandler}/>
-          <Key label={6} onClick={clickHandler}/>
-          <Key label={"×"} onClick={clickHandler}/>
-          <Key label={1} onClick={clickHandler}/>
-          <Key label={2} onClick={clickHandler}/>
-          <Key label={3} onClick={clickHandler}/>
-          <Key label={"-"} onClick={clickHandler}/>
-          <Key label={"CLR"} onClick={clickHandler}/>
-          <Key label={0} onClick={clickHandler}/>
-          <Key label={"="} onClick={clickHandler}/>
-          <Key label={"+"} onClick={clickHandler}/>
+          <Key label={7} onClick={numClickHandler}/>
+          <Key label={8} onClick={numClickHandler}/>
+          <Key label={9} onClick={numClickHandler}/>
+          <Key label={"÷"} onClick={opClickHandler}/>
+          <Key label={4} onClick={numClickHandler}/>
+          <Key label={5} onClick={numClickHandler}/>
+          <Key label={6} onClick={numClickHandler}/>
+          <Key label={"×"} onClick={opClickHandler}/>
+          <Key label={1} onClick={numClickHandler}/>
+          <Key label={2} onClick={numClickHandler}/>
+          <Key label={3} onClick={numClickHandler}/>
+          <Key label={"-"} onClick={opClickHandler}/>
+          <Key label={"CLR"} onClick={clrClickHandler}/>
+          <Key label={0} onClick={numClickHandler}/>
+          <Key label={"="} onClick={eqClickHandler}/>
+          <Key label={"+"} onClick={opClickHandler}/>
         </div>
       </div>
     </div>
