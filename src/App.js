9b6832fb574c3e5b20b3f1b379fe4cf69abd3101
NateDeCod3 <nathanielmanansala18@gmail.com>
2024-10-06 22:31:01 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,119 +1,156 @@
 import './App.css';
 import { useState } from "react";
 
+
+// ---FUNCIONS---
+
+
+// Display Function
 function Display({ display }) {
   return(
     <>{display}</>
   );
 }
 
-function Key({label, onClick}) {
+// Key Function
+function Key({label, onClick, className}) {
   return (
-    <button onClick={onClick}>
-      {label}
+    <button onClick={onClick} className={className}>
+        {label}
     </button>
   );
 }
 
+// App Function
 function App() {
-
   const [disp, setDisp] = useState(0);
   const [num1, setNum1] = useState(null);
   const [num2, setNum2] = useState(null);
   const [op, setOp] = useState(null);
+  const [isNum2, setIsNum2] = useState(false);
 
-
-// 
-
-
+// Number Click Handler
   const numClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    if (op===null) {
-      if (num1===null){
+    if (!isNum2) {
+      if (num1 === null) {
         setNum1(value);
         setDisp(value);
-      }else{
-        setNum1(num1+value);
+      } else {
+        setNum1(num1 + value);
         setDisp(num1 + value);
-
       }
-    }else{
-      if (num2===null){
+    } else {
+      if (num2 === null) {
         setNum2(value);
         setDisp(value);
-      }else{
-        setNum2(num2+value);
+      } else {
+        setNum2(num2 + value);
         setDisp(num2 + value);
-
       }
     }
-    console.log(num1 + '|' + op + '|' + num2);
-  }
+  };
 
+// Operations Click Handler
   const opClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    setOp(value);
-    setDisp(value);
-    console.log(num1 + '|' + op + '|' + num2);
-  }
+    if (num1 !== null) {
+      setOp(value);
+      setIsNum2(true);
+      setDisp(value);
+    }
+  };
 
+// Equal Click Handler
   const eqClickHandler = (e) => {
     e.preventDefault();
-    const value = e.target.innerHTML;
-    console.log(num1 + '|' + op + '|' + num2);
-    if (op==="+"){
-      setDisp(parseInt(num1) + parseInt(num2));
-    }else if (op==="-"){
-      setDisp(parseInt(num1) - parseInt(num2));
-    }else if (op==="×"){
-      setDisp(parseInt(num1) * parseInt(num2));
-    }else if (op==="÷"){
-      setDisp(parseInt(num1) / parseInt(num2));
-    }else{
-      setDisp("ERROR");
+    if (num1 !== null && num2 !== null && op !== null) {
+      let result = null;
+      if (op === "+") {
+        result = parseInt(num1) + parseInt(num2);
+      } else if (op === "-") {
+        result = parseInt(num1) - parseInt(num2);
+      } else if (op === "*") {
+        result = parseInt(num1) * parseInt(num2);
+      } else if (op === "÷") {
+        result = parseInt(num1) / parseInt(num2);
+      } else {
+        result = "ERROR";
     }
-  }
-
-  const clrClickHandler = (e) => {
-    setNum1(null);
+    
+    setDisp(result);
+    setNum1(result);
     setNum2(null);
     setOp(null);
-    setDisp(0);
+    setIsNum2(false);
   }
+};
+
+// Clear Click Handler
+  const clrClickHandler = (e) => {
+    e.preventDefault();
+      setDisp(0);
+      setNum1(null);
+      setNum2(null);
+      setOp(null);
+      setIsNum2(false);  
+};
+
+// Name Click Handler
+  const nameClickHandler = () => {
+    setDisp("Nathaniel Manansala")
+  };
 
 
-// 
-  
+// ---RETURN---
 
-  return (
-    <div className="App">
-      <div className="Calc">
-        <div className="Disp">
-          <Display display={disp} />
-        </div>
-        <div className="Buttons">
-          <Key label={7} onClick={numClickHandler}/>
-          <Key label={8} onClick={numClickHandler}/>
-          <Key label={9} onClick={numClickHandler}/>
-          <Key label={"÷"} onClick={opClickHandler}/>
-          <Key label={4} onClick={numClickHandler}/>
-          <Key label={5} onClick={numClickHandler}/>
-          <Key label={6} onClick={numClickHandler}/>
-          <Key label={"×"} onClick={opClickHandler}/>
-          <Key label={1} onClick={numClickHandler}/>
-          <Key label={2} onClick={numClickHandler}/>
-          <Key label={3} onClick={numClickHandler}/>
-          <Key label={"-"} onClick={opClickHandler}/>
-          <Key label={"CLR"} onClick={clrClickHandler}/>
-          <Key label={0} onClick={numClickHandler}/>
-          <Key label={"="} onClick={eqClickHandler}/>
-          <Key label={"+"} onClick={opClickHandler}/>
-        </div>
+
+return (
+  <div className="App">
+
+    {/* Header */}
+    <div className="Header">
+      Calculator of Nathaniel Manansala - IT3A
+    </div>
+
+    {/* Calculator */}
+    <div className="Calc">
+
+      {/* Calculator Display */}
+      <div className="Disp">
+        <Display display={disp} />
+      </div>
+      
+      {/* Calculator Key Buttons */}
+      <div className="Buttons">
+        <Key label={7} onClick={numClickHandler}/>
+        <Key label={8} onClick={numClickHandler}/>
+        <Key label={9} onClick={numClickHandler}/>
+        <Key label={"÷"} onClick={opClickHandler} className="Operators"/>
+        <Key label={4} onClick={numClickHandler}/>
+        <Key label={5} onClick={numClickHandler}/>
+        <Key label={6} onClick={numClickHandler}/>
+        <Key label={"*"} onClick={opClickHandler} className="Operators"/>
+        <Key label={1} onClick={numClickHandler}/>
+        <Key label={2} onClick={numClickHandler}/>
+        <Key label={3} onClick={numClickHandler}/>
+        <Key label={"-"} onClick={opClickHandler} className="Operators"/>
+        <Key label={"C"} onClick={clrClickHandler} className="Clear"/>
+        <Key label={0} onClick={numClickHandler}/>
+        <Key label={"="} onClick={eqClickHandler}/>
+        <Key label={"+"} onClick={opClickHandler} className="Operators"/>
+      </div>
+
+      {/* Name Button */}
+      <div className="nameButton">
+        <Key label = {'Manansala'} onClick={nameClickHandler}/>
       </div>
     </div>
-  );
+  </div>
+);
+
 }
 
 export default App;
